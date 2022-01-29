573ed08a25dcf519c8501ab0c2e6137ea04de9d1
Mario Zelaya <mariozelaya23@gmail.com>
2022-01-29 10:38:12 -0800

    adding changepassword.php page I just copied and modified a form from general.html, I added connectdb.php and session_start, on header.php I added the link to change password on change password button

---
+++ changepassword.php
@@ -0,0 +1,70 @@
+<!-- this is the header -->
+<?php 
+  include_once 'connectdb.php';
+  session_start();
+
+  include_once'header.php';
+?>
+
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Admin Dashboard
+        <small></small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    <!-- Main content -->
+    <section class="content container-fluid">
+
+      <!--------------------------
+        | Your Page Content Here |
+        -------------------------->
+      <!-- general form elements -->
+          <div class="box box-primary">
+            <div class="box-header with-border">
+              <h3 class="box-title">Change password form</h3>
+            </div>
+            <!-- /.box-header -->
+            <!-- form start -->
+            <form role="form">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Old Password</label>
+                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txtoldpass">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">New Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txtnewpass">
+                </div>
+                <div class="form-group">
+                  <label for="exampleInputPassword1">Confirm Password</label>
+                  <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="txtconfpass">
+                </div>
+              </div>
+              <!-- /.box-body -->
+
+              <div class="box-footer">
+                <button type="submit" class="btn btn-primary" name="btnupdate">Submit</button>
+              </div>
+            </form>
+          </div>
+          <!-- /.box -->
+
+
+
+
+    </section>
+    <!-- /.content -->
+  </div>
+  <!-- /.content-wrapper -->
+
+  <?php 
+    include_once 'footer.php';
+  ?>
