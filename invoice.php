1b900b80c4f55e6752509a9452dcd188dfa90722
Mario Zelaya <mariozelaya23@gmail.com>
2022-03-29 11:30:11 -0700

    building an static invoice

--- invoice.php
+++ invoice.php
@@ -22,7 +22,31 @@ $pdf = new FPDF('P','mm','A4');
 //Mixed format (A3, A4, A5, Letter and Legal) - format of pages
 
 //add new page
-$pdf->AddPage('P');
+$pdf->AddPage();
+
+//$pdf->SetFillColor(123,255,234);
+
+$pdf->SetFont('Arial','B',16);
+$pdf->Cell(80,10,'Server Net Hosting',0,0,'');
+
+$pdf->SetFont('Arial','B',13);
+$pdf->Cell(112,10,'INVOICE',0,1,'C');
+
+$pdf->SetFont('Arial','',8);
+$pdf->Cell(80,5,'Address: Simi Valley, C.A.',0,0,'');
+
+$pdf->SetFont('Arial','',10);
+$pdf->Cell(112,5,'Invoice: #12345',0,1,'C');
+
+$pdf->SetFont('Arial','',8);
+$pdf->Cell(80,5,'Phone number: 805-365-6520',0,0,'');
+
+$pdf->SetFont('Arial','',10);
+$pdf->Cell(112,5,'Date: 03/29/2022',0,1,'C');
+
+$pdf->SetFont('Arial','',8);
+$pdf->Cell(80,5,'Email: info@servernethosting.com',0,1,'');
+$pdf->Cell(80,5,'Website: www.servernethosting.com',0,1,'');
 
 //output the result
 $pdf->Output();
